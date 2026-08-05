f80629270dc6b00ffdd9f7a260850167c0bb69b1
AVS <anselsprague15@gmail.com>
2026-08-05 15:59:03 -0400

    Clean up Arch installer
    Added subtle wait times and split check_linux into 3 sub-functions

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-# TODO: add one function for check and install if not there
+
+# Exit in case of failure
+set -e
+
 # Clear screen
 printf "\033[2J\033[1;1H"
 
 printf "\nWelcome to The CS Launchpad Installer!\n\n"
+sleep 0.5
 
 OS="$(uname -s)"
 
@@ -16,12 +20,33 @@ case "$OS" in
 esac
 
 if [ "$known_os" != "true" ]; then
-    printf "Unknown OS detected: $OS\nAborting."
+    printf "Unknown OS detected: %s\nAborting." "$OS"
     exit 1
 fi
 
-printf "OS detected succesfully!\n\nStarting installation for $os_name...\n\nChecking for prerequisites:\n"
+printf "OS detected succesfully!\n\nStarting installation for %s...\n\nChecking for prerequisites:\n" "$os_name"
+sleep 0.3
 
+
+check_for_command() {
+    if command -v "$1" > /dev/null 2>&1; then
+        printf "\t> %s is installed!\n" "$2"
+        sleep 0.3
+    else
+        printf "%s is not installed. Aborting.\n" "$2"
+        exit 1
+    fi
+}
+
+try_install() {
+    if command -v "$1" > /dev/null 2>&1; then
+        printf "\t> %s: Already installed!\n" "$3"
+        sleep 0.3
+    else
+        printf "\t> Installing %s...\n" "$3"
+        sudo pacman -S -q --noconfirm "$2" && printf "\t> %s succesfully installed!\n" "$3"
+    fi
+}
 
 # Functions are defined BEFORE calling to prevent error
 
@@ -29,16 +54,6 @@ check_linux() {
     # Check if sudo exists
     if ! command -v sudo > /dev/null 2>&1; then
         printf "Sudo not found. Aborting.\n"
-        exit 1
-    fi
-
-    # Check if user has sudo privileges
-    if sudo -n true 2>&1 | grep -q "a password is required\|incorrect password"; then
-        printf "\t> Sudo is installed and User has access!\n"
-    elif sudo -v -n > /dev/null 2>&1 || [ $? -eq 0 ]; then
-        printf "\t>Sudo is installed and User has access!\n"
-    else
-        printf "User does not have access to Sudo. Aborting.\n"
         exit 1
     fi
 
@@ -50,83 +65,78 @@ check_linux() {
         exit 1
     fi
 
-    # Check for Pacman
-    if command -v pacman > /dev/null 2>&1; then
-        printf "\t> Pacman is installed!\n"
-    else
-        printf "Pacman is not installed. Aborting.\n"
-        exit 1
-    fi
+    # Check for Sudo
+    check_for_command sudo Sudo
 
-    printf "\nAll prerequisites found!\n\nChecking for apps to install:\n"
+    # Check for Pacman
+    check_for_command pacman Pacman
+
+    printf "\n[1/3] All prerequisites found!\n\n"
+    sleep 0.5
+}
+
+install_linux() {
+    printf "Refreshing Pacman database if needed. Please enter your password below if prompted.\n\n"
+    sudo pacman -Sy --noconfirm
 
     # Check for Git
-    if command -v git > /dev/null 2>&1; then
-        printf "\t> Git: already installed!\n"
-    else
-        printf "\t> Installing Git...\n"
-        sudo pacman -Sy -q --noconfirm git && printf "\t> Git succesfully installed!\n"
-    fi
+    try_install git git Git
 
     # Check for Python
-    if command -v python3 > /dev/null 2>&1; then
-        printf "\t> Python: already installed!\n"
-    else
-        sudo pacman -Sy -q --noconfirm python3 && printf "\t> Python succesfully installed!\n"
-    fi
+    try_install python python Python
 
     # Check for VS Code
-    if command -v code > /dev/null 2>&1; then
-        printf "\t> VS Code: already installed!\n"
-    else
-        # Install Code OSS with pacman
-        sudo pacman -Sy -q --noconfirm code && printf "\t> VS Code succesfully installed!\n"
-    fi
+    try_install code code "VS Code"
 
     # Check for GitHub CLI
-    if command -v gh > /dev/null 2>&1; then
-        printf "\t> GitHub CLI: already installed!\n"
-    else
-        sudo pacman -Sy -q --noconfirm github-cli && printf "\t> GitHub CLI succesfully installed!\n"
-    fi
+    try_install gh github-cli "GitHub CLI"
 
-    printf "\nInstallation step complete!\n"
+    printf "\n[2/3] Installation step complete!\n"
+    sleep 0.5
+}
 
+configure_linux() {
     printf "\nChecking config files:\n"
+    sleep 0.3
 
 
     # Git name and email
 
     printf "\t> Git:\n"
-    GIT_NAME=$(git config --global user.name)
+    GIT_NAME="$(git config --global user.name)"
     if [ "$GIT_NAME" != "" ]; then
-        printf "\t  - Name: $GIT_NAME\n"
+        printf "\t  - Name: %s\n" "$GIT_NAME"
+        sleep 0.2
     else
         printf "There is not a name in your current Git config. Please enter your name:\n"
-        read name
-        git config --global user.name $name
+        read -r name
+        git config --global user.name "$name"
     fi
 
-    GIT_EMAIL=$(git config --global user.email)
+    GIT_EMAIL="$(git config --global user.email)"
     if [ "$GIT_EMAIL" != "" ]; then
-        printf "\t  - Email: $GIT_EMAIL\n"
+        printf "\t  - Email: %s\n" "$GIT_EMAIL"
+        sleep 0.2
     else
         printf "There is not an email in your current Git config. Please enter your email:\n"
-        read email
-        git config --global user.email $email
-        printf "\t  - Global email set as $email\n"
+        read -r email
+        git config --global user.email "$email"
+        printf "\t  - Global email set as %s\n" "$email"
     fi
+    sleep 0.4
 
     # GH CLI auth status check
     printf "\t> GitHub CLI:\n"
 
     if gh auth status >/dev/null 2>&1; then
         printf "\t  - GitHub CLI is already authenticated!\n"
+        sleep 0.3
     else
         printf "Not logged into GitHub CLI\nPlease sign in on the browser window."
         gh auth login -h github.com -p https -w --skip-ssh-key
     fi
-    
+
+    printf "\n\n[3/3] Everything is configured!\n"
 }
 
 
@@ -139,7 +149,7 @@ check_win() {
 }
 
 case $os_name in
-    Linux) check_linux ;;
+    Linux) check_linux ; install_linux ; configure_linux ;;
     macOS) check_mac ;;
     Windows) check_win ;;
 esac
